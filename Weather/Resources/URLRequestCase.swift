c1e21c1094acaf5bd4a1dafc5f667af185f13ed9
jeongyun-kim <kkanyo04@naver.com>
2024-07-12 10:37:27 +0900

    Feat: URL Router Pattern으로 관리

---
+++ Weather/Resources/URLRequestCase.swift
@@ -0,0 +1,45 @@
+//
+//  URLRequestCase.swift
+//  Weather
+//
+//  Created by 김정윤 on 7/11/24.
+//
+import Foundation
+import Alamofire
+
+enum URLRequestCase {
+    case nowWeather(id: String)
+    case regularWeather(id: String)
+    
+    var baseURL: String {
+        switch self {
+        case .nowWeather:
+            return "https://api.openweathermap.org/data/2.5/"
+        case .regularWeather:
+            return "https://api.openweathermap.org/data/2.5/"
+        }
+    }
+    
+    var endPoint: URL? {
+        switch self {
+        case .nowWeather:
+            guard let url = URL(string: baseURL + "weather") else { return nil }
+            return url
+        case .regularWeather:
+            guard let url = URL(string: baseURL + "forecast") else { return nil }
+            print(url)
+            return url
+        }
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var params: Parameters {
+        switch self {
+        case .nowWeather(let id), .regularWeather(let id):
+            return ["id": id, "appid": APIKey.key]
+        }
+    }
+}
